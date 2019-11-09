52c639e07aa95eb2437e4c6af7522dd22aacd796
Morten Lohne <lohnemorten@gmail.com>
2019-11-09 17:01:54 +0100

    Move pgn2fen to its own module

--- src/main.rs
+++ src/main.rs
@@ -2,101 +2,16 @@ extern crate pgn_reader;
 extern crate shakmaty;
 
 mod tests;
+mod pgn2fen;
 
-use std::{io, error};
-use std::io::Write;
+use std::io;
 use std::env;
-
-use pgn_reader::{Visitor, Skip, BufferedReader, SanPlus, RawHeader};
-use shakmaty::fen::Fen;
-use std::error::Error;
-use shakmaty::{Position, fen};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     match args.get(1).map(String::as_str) {
-        Some("png2fen") => pgn2fen(&mut io::stdin(), &mut io::stdout()),
+        Some("png2fen") => pgn2fen::pgn2fen(&mut io::stdin(), &mut io::stdout()),
         Some(s) => eprintln!("{} is not a pgn-tools command.", s),
         None => eprintln!("Usage: pgn-tools png2fen")
     }
-}
-
-struct Pgn2FenVisitor<'w, W> {
-    chess: Result<shakmaty::Chess, Box<dyn error::Error>>,
-    writer: &'w mut W,
-}
-
-impl<'w, W> Pgn2FenVisitor<'w, W> {
-    fn new(writer: &'w mut W) -> Self {
-        Pgn2FenVisitor { chess: Ok(shakmaty::Chess::default()), writer }
-    }
-}
-
-impl <'w, W: Write> Visitor for Pgn2FenVisitor<'w, W> {
-    type Result = ();
-
-    fn header(&mut self, key: &[u8], value: RawHeader<'_>) {
-        if key == "FEN".as_bytes() {
-            let key_str = std::str::from_utf8(value.0).unwrap();
-            match key_str.parse::<Fen>() {
-                Ok(setup) =>
-                    self.chess = setup.position()
-                    .map_err(|err| Box::new(err) as Box<dyn Error>),
-                Err(err) => self.chess = Err(Box::new(err)),
-            }
-        }
-    }
-
-    fn begin_game(&mut self) {
-        self.chess = Ok(shakmaty::Chess::default());
-    }
-
-    fn san(&mut self, san_plus: SanPlus) {
-        if let Ok(_) = self.chess {
-            match san_plus.san.to_move(self.chess.as_ref().unwrap()) {
-                Ok(mv) => self.chess.as_mut().unwrap().play_unchecked(&mv),
-                Err(err) => {
-                    self.chess = Err(Box::new(err));
-                    return;
-                },
-            }
-            let fen = fen::fen(self.chess.as_ref().unwrap());
-            writeln!(self.writer, "{}", fen).unwrap();
-        }
-
-    }
-
-    fn begin_variation(&mut self) -> Skip {
-        Skip(true)
-    }
-
-    fn end_game(&mut self) -> Self::Result {
-        ()
-    }
-}
-
-pub fn pgn2fen<R: io::Read, W: io::Write>(reader: &mut R, writer: &mut W) {
-    let mut pgn_reader = BufferedReader::new(reader);
-    let mut visitor = Pgn2FenVisitor::new(writer);
-
-    let mut successes: u64 = 0;
-    let mut failures: u64 = 0;
-
-    loop {
-        let result = pgn_reader.read_game(&mut visitor);
-
-        match result {
-            Ok(None) => break,
-            _ => (),
-        }
-
-        match visitor.chess {
-            Ok(_) => successes += 1,
-            Err(ref err) => {
-                eprintln!("{}", err);
-                failures += 1;
-            },
-        }
-    }
-    eprintln!("Parsed {}/{} pgns successfully", successes, successes + failures);
 }
--- src/tests/pgn2fen_tests.rs
+++ src/tests/pgn2fen_tests.rs
@@ -1,6 +1,7 @@
 use std::fs;
 use std::io;
 use std::io::Read;
+use crate::pgn2fen;
 
 #[test]
 fn pgn2fen_test() {
@@ -8,7 +9,7 @@ fn pgn2fen_test() {
     let expected_output = fs::File::open("src/tests/rasher_vs_curryfish_fens.txt").unwrap();
 
     let mut output = io::Cursor::new(Vec::new());
-    crate::pgn2fen(&mut input, &mut output);
+    pgn2fen::pgn2fen(&mut input, &mut output);
 
     assert!(expected_output.bytes().map(Result::unwrap)
         .eq(output.into_inner().into_iter()));
@@ -20,7 +21,7 @@ fn pgn2fen_test2() {
     let expected_output = fs::File::open("src/tests/TCEC_Season_16_-_Superfinal_fens.txt").unwrap();
 
     let mut output = io::Cursor::new(Vec::new());
-    crate::pgn2fen(&mut input, &mut output);
+    pgn2fen::pgn2fen(&mut input, &mut output);
 
     assert!(expected_output.bytes().map(Result::unwrap)
         .eq(output.into_inner().into_iter()));
